0d822f6510c851e6824ab0aecd20d307ea46879c
Alexander Kimani <a906k620@ku.edu>
2016-02-25 01:14:54 -0600

    Initial commit changecss.js change border and background

---
+++ jscssmanipulation/changecss.js
@@ -0,0 +1,14 @@
+
+
+function change()
+{
+    var bdrRed = document.getElementById("bdred");
+    var bdrGrn = document.getElementById("bdred");
+    var bdrBlue = document.getElementById("bdred");
+
+    var x = document.getElementById("main");
+
+
+    x.style.borderColor = "red";
+    x.style.backgroundColor = "green";
+}
