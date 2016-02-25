e7fe13d3d699d902e29bfe4be857ee6a90a0fd65
Alexander Kimani <a906k620@ku.edu>
2016-02-25 02:50:59 -0600

    Add border width manipulate with javascript

--- jscssmanipulation/changecss.js
+++ jscssmanipulation/changecss.js
@@ -5,6 +5,8 @@ function change()
     var bdrRed = document.getElementById("bred").value;
     var bdrGrn = document.getElementById("bgreen").value;
     var bdrBlue = document.getElementById("bblue").value;
+    var bdrWidth = document.getElementById("bwidth").value;
+
     var bgRed = document.getElementById("bgred").value;
     var bgGreen = document.getElementById("bggreen").value;
     var bgBlue = document.getElementById("bgblue").value;
@@ -15,4 +17,6 @@ function change()
     x.style.borderColor = "rgb("+bdrRed+","+bdrGrn+","+bdrBlue+")";
 
     x.style.backgroundColor = "rgb("+bgRed+","+bgGreen+","+bgBlue+")";
+
+    x.style.borderWidth = bdrWidth;
 }
