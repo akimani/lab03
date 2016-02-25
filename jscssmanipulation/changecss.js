420f39e07b6a170ac17ca3f649a3d49e63a7a493
Alexander Kimani <a906k620@ku.edu>
2016-02-25 02:20:20 -0600

    Change border-color in javascript

--- jscssmanipulation/changecss.js
+++ jscssmanipulation/changecss.js
@@ -2,13 +2,15 @@
 
 function change()
 {
-    var bdrRed = document.getElementById("bdred");
-    var bdrGrn = document.getElementById("bdred");
-    var bdrBlue = document.getElementById("bdred");
+    var bdrRed = document.getElementById("bred").value;
+    var bdrGrn = document.getElementById("bgreen").value;
+    var bdrBlue = document.getElementById("bblue").value;
+    var bgRed = document.getElementById("bgred").value;
+    var bgGreen = document.getElementById("bggreen").value;
+    var bgBlue = document.getElementById("bgblue").value;
 
     var x = document.getElementById("main");
 
 
-    x.style.borderColor = "red";
-    x.style.backgroundColor = "green";
+    x.style.borderColor = "rgb("+bdrRed+","+bdrGrn+","+bdrBlue+")";
 }
